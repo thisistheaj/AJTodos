7e14471a20cae005dbf480828ff2e38a4184675b
AJ Beckner <ajbeckner@gmail.com>
2016-04-10 03:07:36 -0400

    Split Controller files

--- www/js/ListViewController.js
+++ www/js/ListViewController.js
@@ -1,4 +1,4 @@
-angular.module('AJTodos.controllers', [])
+angular.module('AJTodos.ListViewController', [])
 
   .controller('ListViewController', function($scope,$ionicPopup,$stateParams,$http) {
     $scope.userName = $stateParams.userName;
@@ -62,40 +62,5 @@ angular.module('AJTodos.controllers', [])
         ]
       });
     };
-
-  })
-
-  .controller('LoginViewController', function($scope,$state,$http) {
-    $scope.userName = "";
-    $scope.password = "";
-    $scope.wrongPw = "";
-
-    $scope.login = function (un, pw) {
-      $http({
-        method: 'GET',
-        url: 'http://localhost:3000/userName/' + un
-      }).then(function successCallback(response) {
-        if (response.data.length > 0) {
-          if (response.data[0].password === pw){
-            $state.go('listview',{userName:un});
-          } else {
-            alert("error: incorrect password");
-          }
-        } else {
-          $http({
-            method: 'POST',
-            url: 'http://localhost:3000',
-            headers: {'Content-Type': 'application/json'},
-            data: JSON.stringify({userName:un,password:pw,data:[]})
-          }).then(function successCallback(response) {
-            $state.go('listview',{userName:un});
-          }, function errorCallback(response) {
-            alert("Error: could not connect");
-          });
-        }
-      }, function errorCallback(response) {
-        alert("Error: could not connect");
-      });
-    }
 
   });
